57141a18cf5ff16af018ff5251ed1b44bbe307c2
vijaykumarjonnakuti0-commits <vijaykumarjonnakuti0@gmail.com>
2026-07-15 11:48:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleAnswer(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
